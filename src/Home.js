b64cdb38da207a82658d3947778c9024852801c3
William Rivera <william.pr07@hotmail.com>
2019-08-30 01:43:51 -0400

    add image

--- src/Home.js
+++ src/Home.js
@@ -10,15 +10,19 @@ class Home extends Component {
                     <div className="col-sm-12">
                         <div className="jumbotron">
                             <h1 className="display-4">Single Page App</h1>
-                            <p className="lead">A single-page application is an application that loads a single HTML 
-                                    page and all the necessary assets (such as JavaScript and CSS) required
-                                    for the application to run. Any interactions with the page or subsequent
-                                    pages do not require a round trip to the server which means the page is
-                                    not reloaded.</p>
+                            <figure>
+                                <img className="img-fluid" src="panda-hanging-rode.png" alt="Panda" />
+                            </figure>
+                            <p className="lead">
+                                A single-page application is an application that loads a single HTML 
+                                page and all the necessary assets (such as JavaScript and CSS) required
+                                for the application to run. Any interactions with the page or subsequent
+                                pages do not require a round trip to the server which means the page is
+                                not reloaded.</p>
                             <hr className="my-4" />
                             <p>For more information visit react website.</p>
                             <a className="btn btn-warning btn-lg" href="http://reactjs.org" role="button">Learn more</a>
-                        </div>         
+                        </div>
                     </div>            
                 </div>
             </div>        
