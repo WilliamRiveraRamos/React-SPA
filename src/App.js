270e381294267e899daab496deb6d2c52282a42f
William Rivera <william.pr07@hotmail.com>
2019-09-13 20:11:20 -0400

    Fix className and dateTime tags

--- src/App.js
+++ src/App.js
@@ -12,55 +12,55 @@ import {
   import About from './About';
   import Contact from './Contact';
 
-  class App extends Component {
-    render() {
-      return (
-        <HashRouter>
-          <nav className="navbar navbar-expand-lg navbar-light bg-light">        
-            <NavLink className="navbar-brand" to="/">React.js SPA</NavLink>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-              <span className="navbar-toggler-icon"></span>
-            </button>
-                    
-            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-              <div className="navbar-nav">
-                <NavLink className="nav-item nav-link" to="/">Home</NavLink>
-                <NavLink className="nav-item nav-link" to="/Blog">Blog</NavLink>
-                <NavLink className="nav-item nav-link" to="/About">About</NavLink>
-                <NavLink className="nav-item nav-link" to="/Contact">Contact</NavLink>
-              </div>
-            </div>
-          </nav>
+  
+class App extends Component {
+	render() {
+		return (
+			<HashRouter>
+				<nav className="navbar navbar-expand-lg navbar-light bg-light">        
+					<NavLink className="navbar-brand" to="/">React.js SPA</NavLink>
+					<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+					<span className="navbar-toggler-icon"></span>
+					</button>
+							
+					<div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+					<div className="navbar-nav">
+						<NavLink className="nav-item nav-link" to="/">Home</NavLink>
+						<NavLink className="nav-item nav-link" to="/Blog">Blog</NavLink>
+						<NavLink className="nav-item nav-link" to="/About">About</NavLink>
+						<NavLink className="nav-item nav-link" to="/Contact">Contact</NavLink>
+					</div>
+					</div>
+				</nav>
 
-          <div className="container">
-            <h1 className="mt-3 MainTitle">
-              <img src="/react-logo.png" className="mr-2"
-              alt="React JS logo"
-              width="64"
-              height="64" />
-              React SPA
-            </h1>
-          </div>
-          
-          <div>
-            <Route exact path="/" component = { Home } />           
-            <Route path="/blog" component = { Blog } />
-            <Route path="/about" component = { About } />
-            <Route path="/contact" component = { Contact } />
-          </div>
+				<div className="container">
+					<h1 className="mt-3 MainTitle">
+					<img src="/react-logo.png" className="mr-2"
+					alt="React JS logo"
+					width="64"
+					height="64" />
+					React SPA
+					</h1>
+				</div>
+			
+				<div>
+					<Route exact path="/" component = { Home } />           
+					<Route path="/blog" component = { Blog } />
+					<Route path="/about" component = { About } />
+					<Route path="/contact" component = { Contact } />
+				</div>
 
-          <Footer />
+			<Footer />
+			</HashRouter>        
+		)
+	}
+}
 
-          </HashRouter>        
-        )
-      }
-    }
-
-    const Footer = () => {
-      return(
-        <footer className="fixed-bottom">
-          <p>&copy;2019</p>
-        </footer>
-      )
-    }
-    export default App;
+const Footer = () => {
+	return(
+		<footer className="fixed-bottom">
+		<p>&copy;2019</p>
+		</footer>
+	)
+}
+export default App;
--- src/Blog.js
+++ src/Blog.js
@@ -6,7 +6,7 @@ class Blog extends Component {
             <div className="container">
                 <div className="row">
                     <div className="col-sm-12">
-                        <div class="shadow mb-5 bg-white rounded">
+                        <div className="shadow mb-5 bg-white rounded">
                             <div className="card mt-4">
                                 <div className="card-body">                            
                                     <main>
@@ -14,7 +14,7 @@ class Blog extends Component {
                                             <header>
                                                 <h1>My First Post</h1>
                                                 <small>
-                                                    <time datetime="2008-02-14 20:00" className="text-muted">
+                                                    <time dateTime="2008-02-14 20:00" className="text-muted">
                                                         Published: 2019-05-12 21:00</time>
                                                 </small>
                                                 <small>
